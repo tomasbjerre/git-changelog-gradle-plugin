c57564c99e94ae36de137738be24fbffbc8850d4
Tomas Bjerre <tomas.bjerre85@gmail.com>
2016-01-31 12:15:10 +0100

    lib 1.13

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./gradlew clean cE build install eclipse
+./gradlew clean cE build install eclipse || exit 1
 cd git-changelog-gradle-plugin-example
 ./gradlew generateChangelogs -i --stacktrace
 cp CHANGELOG.md ..
