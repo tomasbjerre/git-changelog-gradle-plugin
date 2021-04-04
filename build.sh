f1da2169260afa829ab31dd55aea85b96e4b0b21
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-04-04 17:46:18 +0200

    new build script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./gradlew clean cE build install eclipse || exit 1
-cd git-changelog-gradle-plugin-example
-./gradlew generateChangelogs -i --stacktrace -DGITHUB_OAUTH2TOKEN=$GITHUB_OAUTH2TOKEN -DGITLAB_TOKEN=$GITLAB_TOKEN || exit 1
-cp CHANGELOG.md ..
